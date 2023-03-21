17625f281b964ca0f33d9e61e2c137ed2861ac48
Dabbyt <dabbyt34@gmail.com>
2023-03-21 23:45:46 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body style= "background-color: #18122B">
+    <div class="container">
+        <section class="section__hero text-center">
+            <img class="p-pic" src={{ asset('image/bytganteng.JPG') }}>
+            <h1>
+                Greeting Friends!! Dabbyt Here, 
+            </h1>
+            <h2>   
+                a Designer, Photographer from Batam. I do Design such as poster, etc. And Wedding Photographer.
+            </h2>
+        </section>
+    </div>
+</body>
+</html>
